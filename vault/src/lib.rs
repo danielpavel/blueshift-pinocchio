b389b2c17c98eccbcc973fb6402d5b61e0586123
Daniel Pavel <daniel.pavel19@gmail.com>
2025-08-04 15:51:04 +0300

    withdraw

--- vault/src/lib.rs
+++ vault/src/lib.rs
@@ -9,33 +9,22 @@ entrypoint!(process_instruction);
 nostd_panic_handler!();
 
 pub mod instructions;
-pub use instructions::*
+pub use instructions::*;
 
 // Program ID
 pub const ID: Pubkey = [
-    0x0f, 0x1e, 0x6b, 0x14, 0x21, 0xc0, 0x4a, 0x07,
-    0x04, 0x31, 0x26, 0x5c, 0x19, 0xc5, 0xbb, 0xee,
-    0x19, 0x92, 0xba, 0xe8, 0xaf, 0xd1, 0xcd, 0x07,
-    0x8e, 0xf8, 0xaf, 0x70, 0x47, 0xdc, 0x11, 0xf7,
+    0x0f, 0x1e, 0x6b, 0x14, 0x21, 0xc0, 0x4a, 0x07, 0x04, 0x31, 0x26, 0x5c, 0x19, 0xc5, 0xbb, 0xee,
+    0x19, 0x92, 0xba, 0xe8, 0xaf, 0xd1, 0xcd, 0x07, 0x8e, 0xf8, 0xaf, 0x70, 0x47, 0xdc, 0x11, 0xf7,
 ];
 
 fn process_instruction(
-    _program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]
-    ) -> ProgramResult {
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
     match instruction_data.split_first() {
         Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
         Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
-        _ => Err(ProgramError::InvalidInstructionData)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        _ => Err(ProgramError::InvalidInstructionData),
     }
 }
